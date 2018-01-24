3689874c7e2fd9efefe10fe21c51079f1c75a2bb
Megan Poosangtongchai <mpoosangtongchai@gmail.com>
2018-01-24 04:14:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,15 +23,7 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  return parseInt();
-}
-
-function makeInt(n) {
   return parseInt(n, 10);
-}
-
-function makeInt() {
-  return ;
 }
 
 function preserveDecimal(n) {
